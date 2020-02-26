b8e532b1536138e191ce07e9eba4e27dcc8f688d
jieliuhk <jliu155@dons.usfca.edu>
2020-02-26 15:44:16 -0800

    add code

---
+++ src/AES.java
@@ -0,0 +1,25 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class AES {
+    private static SecretKeySpec secretKey;
+
+    public static void setKey(String myKey) {
+        byte[] key = myKey.getBytes();
+        secretKey = new SecretKeySpec(key, "AES");
+    }
+
+    public static String encrypt(String strToEncrypt, String key) {
+        try {
+            setKey(key);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/Demo.java
@@ -0,0 +1,23 @@
+import java.util.Base64;
+
+public class Demo {
+
+    public static String MyHash(String input) {
+        return "";
+    }
+
+    public static String getAESCipher(String plaintext) {
+        String key = "12343212we321qwe";
+        return AES.encrypt(plaintext, key);
+    }
+
+    public static void main(String[] args) {
+        String input = "abcd";
+        String res = getAESCipher(input);
+        System.out.println("input String size: " + input.length());
+        System.out.println("base64 byte size: " + Base64.getEncoder().encode(input.getBytes()).length);
+        System.out.println("base64 String: " + Base64.getEncoder().encodeToString(input.getBytes()));
+        System.out.println(Base64.getDecoder().decode(res).length);
+        System.out.println(res);
+    }
+}
