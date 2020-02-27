92df17b3bc61ca3e14285c22758988d9d7a374c1
jieliuhk <jliu155@dons.usfca.edu>
2020-02-26 17:07:37 -0800

    add AES interface

--- src/AES.java
+++ src/AES.java
@@ -1,7 +1,6 @@
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 
 public class AES {
     private static SecretKeySpec secretKey;
@@ -11,12 +10,42 @@ public class AES {
         secretKey = new SecretKeySpec(key, "AES");
     }
 
-    public static String encrypt(String strToEncrypt, String key) {
+    private static void setKey(byte[] myKey) {
+        secretKey = new SecretKeySpec(myKey, "AES");
+    }
+
+    /**
+     * Encrypt plaintext into cipher using AES/ECB
+     *
+     * @param strToEncrypt: plaintext need to be encrypt, human readable text like "hello"
+     * @param key:          the key use to encrypt plaintext, can be any string
+     * @return cipher text represented by byte array
+     */
+    public static byte[] encrypt(String strToEncrypt, String key) {
         try {
             setKey(key);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+            return cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Encrypt plaintext into cipher using AES/ECB
+     *
+     * @param plaintext: plaintext need to be encrypt, binary sequence
+     * @param key:       the key use to encrypt plaintext, binary sequence
+     * @return cipher text represented by byte array
+     */
+    public static byte[] encrypt(byte[] plaintext, byte[] key) {
+        try {
+            setKey(key);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return cipher.doFinal(plaintext);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
--- src/MyHash.java
+++ src/MyHash.java
@@ -1,22 +1,23 @@
 import java.util.Base64;
 
-public class Demo {
-    public static String MyHash(String input) {
+public class MyHash {
+    public static String getHash(String input) {
+        //TODO: implement this
         return "";
     }
 
-    public static String getAESCipher(String plaintext) {
+    public static byte[] getAESCipher(String plaintext) {
         String key = "12343212we321qwe";
         return AES.encrypt(plaintext, key);
     }
 
     public static void main(String[] args) {
-        String input = "abcd";
-        String res = getAESCipher(input);
+        String input = "hello world";
+        byte[] res = getAESCipher(input);
         System.out.println("input String size: " + input.length());
         System.out.println("base64 byte size: " + Base64.getEncoder().encode(input.getBytes()).length);
         System.out.println("base64 String: " + StringUtil.encodeToString(input.getBytes()));
-        System.out.println(Base64.getDecoder().decode(res).length);
-        System.out.println(res);
+        System.out.println(res.length);
+        System.out.println(StringUtil.encodeToString(res));
     }
 }
